21dbb09a564b0ecd8ae1b1e63409288188c4fdeb
Rajendra <itsrajendrapatel@gmail.com>
2024-04-07 16:42:30 +0530

    Signup Done with mailing part

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,45 @@
+import User from "@/models/user.model";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        VerifyTokenExpiry: Date.now() + 3600000, // 1 hours
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgetPasswordToken: hashedToken,
+        forgetPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "459015312be77a",
+        pass: "ec1c67a27397b1",
+      },
+    });
+
+    const mailingOptions = {
+      from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
+      to: "bar@example.com, baz@example.com",
+      subject: "Hello ✔",
+      text: "Hello world?",
+      html: "<b>Hello world?</b>",
+    };
+
+    const info = await transporter.sendMail(mailingOptions);
+
+    return info;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
---
+++ src/models/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    require: [true, "Username is Required"],
+    unique: [true, "Username should be unique"],
+  },
+  email: {
+    type: String,
+    require: [true, "Username is Required"],
+    unique: [true, "Username should be unique"],
+  },
+  password: {
+    type: String,
+    require: [true, "Username is Required"],
+  },
+  isVerified: {
+    type: Boolean,
+    require: [true, "Username is Required"],
+  },
+  isAdmin: {
+    type: Boolean,
+    require: [true, "isAdmin is Required"],
+  },
+  forgetPasswordToken: String,
+  forgetPasswordTokenExpiry: Date,
+  verifyToken: String,
+  VerifyTokenExpiry: Date,
+});
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
